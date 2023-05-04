1c09dca1b9593826023b9ddcac0db537c7ddcfc9
daonguyen207 <54179901+daonguyen207@users.noreply.github.com>
2023-05-04 17:43:33 +0700

    Add files via upload

---
+++ examples/code/async/async.ino
@@ -0,0 +1,18 @@
+#define IOT47_DS18B20_PIN 8  //nếu dùng các chân A thì A0=14 A1=15 A2=16 A3=17 A4=18 A5=19
+#include "i47_async_ds18b20.h"
+
+void DS18B20_CALLBACK(float temp)
+{
+  Serial.println(temp);
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+  ds18b20_begin(DS18B20_CALLBACK);
+}
+void loop() 
+{
+  ds18b20_read();
+  delay(1000);
+}
